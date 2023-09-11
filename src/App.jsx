c152b4e5fabf27459d67acee94a4c0de6c4e6d92
hmRamirez <tohmr@yahoo.com.ar>
2023-09-11 20:32:10 -0300

    calculadora sin funcionalidad - primer commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./app.css";
+import Boton from "./Boton";
+//import fcc from "./assets/react-logo.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="contenedor-calculadora">
+        <div className="fila">
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className="fila"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Boton.jsx
@@ -0,0 +1,20 @@
+import "./boton.css";
+
+function Boton(props) {
+  //
+  const esOperador = (valor) => {
+    return isNaN(valor) && valor != "." && valor != "=";
+  };
+
+  return (
+    <div
+      className={`boton-contenedor ${
+        esOperador(props.children) ? "operador" : ""
+      }`.trim()}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Boton;
